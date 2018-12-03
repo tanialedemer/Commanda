78ebb1d7c4df5d1465dfb630e79e87181149b075
tanialedemer <37988368+tanialedemer@users.noreply.github.com>
2018-12-03 13:28:49 -0300

    menu

---
+++ Commanda/Commanda/Cells/CategoriaCell.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Commanda.Cells
+{
+    public class CategoriaCell : ViewCell
+    {
+        public CategoriaCell()
+        {
+            var categoriaLabel = new Label
+        {
+            HorizontalTextAlignment = TextAlignment.Start,
+            FontSize = 20
+
+        };
+
+        categoriaLabel.SetBinding(Label.TextProperty, new Binding("categoria"));
+
+     
+
+            View = new StackLayout
+            {
+                Children = { categoriaLabel},
+                Orientation = StackOrientation.Horizontal
+            };
+}
+    }
+}
--- Commanda/Commanda/Pages/PedidoPage.xaml.cs
+++ Commanda/Commanda/Pages/PedidoPage.xaml.cs
@@ -3,25 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Net.Http;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Newtonsoft.Json;
+using Commanda.Models;
 
 namespace Commanda.Pages
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class PedidoPage : ContentPage
 	{
+        private List<CategoryResponse> categorias;
         private int id_mesa;
 
         public PedidoPage ()
 		{
 			InitializeComponent ();
-		}
+            lstCategorias.ItemTemplate = new DataTemplate(typeof(Cells.CategoriaCell));
+            this.LoadCategorias();
+        }
 
         public PedidoPage(int id_mesa)
         {
             this.id_mesa = id_mesa;
         }
+
+        private async void LoadCategorias()
+        {
+            string result;
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("http://181.123.10.51");
+                string url = string.Format("/app/command/ProductAPI.php");
+                var response = await client.GetAsync(url);
+                result = response.Content.ReadAsStringAsync().Result;
+            }
+            catch (Exception)
+            {
+                await DisplayAlert("Error", "No hay conexion", "Aceptar");
+                return;
+            }
+
+            categorias = JsonConvert.DeserializeObject<List<CategoryResponse>>(result);
+            lstCategorias.ItemsSource = categorias;
+        }
+
+
     }
 }
